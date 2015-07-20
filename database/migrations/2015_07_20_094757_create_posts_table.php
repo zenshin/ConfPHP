69e8cef23ef51667f53e4fc06984ef8d7b5231d0
zenshin <thomasmperrot@gmail.com>
2015-07-20 12:12:14 +0200

    first commit

---
+++ database/migrations/2015_07_20_094757_create_posts_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+//            $table->integer('user_id')->unsigned()->nullable();
+            $table->string('title', 100);
+            $table->string('slug',100);
+            $table->string('excerpt',60);
+            $table->text('content');
+            $table->dateTime('date_start');
+            $table->dateTime('date_end');
+            $table->string('thumbnail_link');
+            $table->string('url_site');
+            $table->enum('status', ['publish', 'unpublish'])->default('unpublish');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('SET NULL');
+            $table->softDeletes();
+            $table->timestamps();
+
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+        Schema::drop('posts');
+
+    }
+}
---
+++ database/migrations/2015_07_20_095126_create_comments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('email',100)->nullable();
+            $table->text('message');
+            $table->enum('status', ['publish', 'unpublish'])->default('unpublish');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('comments');
+    }
+}
